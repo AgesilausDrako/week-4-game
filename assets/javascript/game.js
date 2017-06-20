29c51d646fe326603f6a4debbc4f67bb077c206c
AgesilausDrako <agesilaus@yahoo.com>
2017-06-20 15:24:48 -0400

    added click function on crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,11 +21,27 @@ function initializeGame (){
 	//Function for generating the Crystal number
 	function crystalNumGenerator () {
 		redCrystal = Math.floor(Math.random() * 12 + 1);
+		redCrystal = parseInt(redCrystal);
+		$("#redCrystal").val(redCrystal);
 		blueCrystal = Math.floor(Math.random() * 12 + 1);
+		blueCrystal = parseInt(blueCrystal);
+		$("#blueCrystal").val(blueCrystal);
 		greenCrystal = Math.floor(Math.random() * 12 + 1);
+		greenCrystal = parseInt(greenCrystal);
+		$("#greenCrystal").val(greenCrystal);
 		yellowCrystal = Math.floor(Math.random() * 12 + 1);
+		yellowCrystal = parseInt(yellowCrystal);
+		$("#yellowCrystal").val(yellowCrystal);
 		}
 		crystalNumGenerator();
+}
+
+function reset () {
+	redCrystal = "";
+	blueCrystal = "";
+	greenCrystal = "";
+	yellowCrystal = "";
+	initializeGame();
 }
 
 
@@ -34,20 +50,22 @@ $("document").ready(function (){
 	//Sets up all the random numbers
 	initializeGame();
 
-	
+	$(".crystal").on("click", function(){
+		scoreNum+=this.value;
+		$("#results").html(scoreNum);
+	});
 
 
 
+	//Resets game if player exceeds random number and updates losses
+	if(scoreNum > randomNum) {
+		losses+=1;
+		reset();
+	}
 
-
-
-
-
-
-
-
-
-
-
-
+	//Resets game if player matches random number and updates wins
+	if (scoreNum === randomNum) {
+		wins+=1;
+		reset();
+	}
 });
