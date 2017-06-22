6ac04a4c790ce33019db0da3ad71236af9b2c4cb
AgesilausDrako <agesilaus@yahoo.com>
2017-06-22 15:50:39 -0400

    deleted the crystal variables for more efficient code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,72 +4,76 @@ $("document").ready(function (){
 	//Random number variable to store the random number
 	var randomNum = 0;
 
+	//Store the random number assigned to each crystal at game's outset
+	var randomCrystal = 0;
+
 	//Stores the total of the player's crystal numbers
 	var scoreNum = 0;
-	var newNum = 0;
 
-	//Store the random number assigned to each crystal at game's outset
-	var redCrystal = 0;
-	var blueCrystal = 0;
-	var greenCrystal = 0;
-	var yellowCrystal = 0;
+	//Stores the number from each click event
+	var newNum = 0;
 
 	//Variables for storing total number of wins and losses
 	var wins = 0;
 	var losses = 0;
+
+	//Logs the initial win/loss scores
 	$("#wins").html(wins);
 	$("#losses").html(losses);
-
-	//Function for a random number between 1 and 12
+	
+	//Initializes the game and creates random numbers for variables
 	function initializeGame (){
+		//Creates a random number between 19 and 120
 		randomNum = Math.floor(Math.random() * (120 - 19) + 19);
 		$("#number-box").html(randomNum);
-		//Function for generating the Crystal number
+		//Function for generating the Crystal number between 1 and 12
 		function crystalNumGenerator () {
-			redCrystal = Math.floor(Math.random() * 12 + 1);
-			$("#redCrystal").val(redCrystal);
-			blueCrystal = Math.floor(Math.random() * 12 + 1);
-			$("#blueCrystal").val(blueCrystal);
-			greenCrystal = Math.floor(Math.random() * 12 + 1);
-			$("#greenCrystal").val(greenCrystal);
-			yellowCrystal = Math.floor(Math.random() * 12 + 1);
-			$("#yellowCrystal").val(yellowCrystal);
-			}
+			randomCrystal = Math.floor(Math.random() * 12 + 1);
+		}
+		function crystalValAssigner () {
 			crystalNumGenerator();
+			$("#redCrystal").val(randomCrystal);
+			crystalNumGenerator();
+			$("#blueCrystal").val(randomCrystal);
+			crystalNumGenerator();
+			$("#greenCrystal").val(randomCrystal);
+			crystalNumGenerator();
+			$("#yellowCrystal").val(randomCrystal);
+			}
+			crystalValAssigner();
 	}
-
-	//Sets up all the random numbers
-	initializeGame();
-
-	$(".crystal").on("click", function(){
+	
+	//Changes strings into integers, adds them to the score, and resets game
+	//when conditions are met
+	function crystalClick (){
 		newNum = parseInt(this.value);
 		scoreNum += newNum;
 		$("#results").html(scoreNum);
-		//Resets game if player exceeds random number and updates losses
-		if (scoreNum > randomNum) {
-			losses++;
-			$("#losses").html(losses);
-			reset();
+			//Resets game if player exceeds random number and updates losses
+			if (scoreNum > randomNum) {
+				losses++;
+				$("#losses").html(losses);
+				reset();
+			}
+			//Resets game if player matches random number and updates wins
+			if (scoreNum === randomNum) {
+				wins++;
+				$("#wins").html(wins);
+				reset();
+			}
 		}
 
-		//Resets game if player matches random number and updates wins
-		if (scoreNum === randomNum) {
-			wins++;
-			$("#wins").html(wins);
-			reset();
-		}
-	});
+	//Assigns the click handler
+	$(".crystal").click(crystalClick);
 
 	//Reset function to clear the crysal variables and reinitialize the game
 	function reset () {
-		redCrystal = 0;
-		blueCrystal = 0;
-		greenCrystal = 0;
-		yellowCrystal = 0;
 		newNum = 0;
 		scoreNum = 0;
 		$("#results").html(scoreNum);
 		initializeGame();
 	}
-	
+
+	//Begins the game
+	initializeGame();
 });
