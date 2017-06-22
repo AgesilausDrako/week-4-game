deb2dc1679928b71ba808249389245808f40d4f2
AgesilausDrako <agesilaus@yahoo.com>
2017-06-22 11:28:57 -0400

    reset fixed and wins/losses logged to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,23 @@
 $("document").ready(function (){
 
 	//Random number variable to store the random number
-	var randomNum = "";
+	var randomNum = 0;
 
 	//Stores the total of the player's crystal numbers
-	var scoreNum = "";
+	var scoreNum = 0;
+	var newNum = 0;
 
 	//Store the random number assigned to each crystal at game's outset
-	var redCrystal = "";
-	var blueCrystal = "";
-	var greenCrystal = "";
-	var yellowCrystal = "";
+	var redCrystal = 0;
+	var blueCrystal = 0;
+	var greenCrystal = 0;
+	var yellowCrystal = 0;
 
 	//Variables for storing total number of wins and losses
-	var wins = "";
-	var losses = "";
+	var wins = 0;
+	var losses = 0;
+	$("#wins").html(wins);
+	$("#losses").html(losses);
 
 	//Function for a random number between 1 and 12
 	function initializeGame (){
@@ -35,32 +38,38 @@ $("document").ready(function (){
 			crystalNumGenerator();
 	}
 
-	function reset () {
-		redCrystal = "";
-		blueCrystal = "";
-		greenCrystal = "";
-		yellowCrystal = "";
-		initializeGame();
-	}
-
 	//Sets up all the random numbers
 	initializeGame();
 
 	$(".crystal").on("click", function(){
-		scoreNum += this.value;
+		newNum = parseInt(this.value);
+		scoreNum += newNum;
 		$("#results").html(scoreNum);
+		//Resets game if player exceeds random number and updates losses
+		if (scoreNum > randomNum) {
+			losses++;
+			$("#losses").html(losses);
+			reset();
+		}
+
+		//Resets game if player matches random number and updates wins
+		if (scoreNum === randomNum) {
+			wins++;
+			$("#wins").html(wins);
+			reset();
+		}
 	});
 
-
-	//Resets game if player exceeds random number and updates losses
-	if(scoreNum > randomNum) {
-		losses+=1;
-		reset();
+	//Reset function to clear the crysal variables and reinitialize the game
+	function reset () {
+		redCrystal = 0;
+		blueCrystal = 0;
+		greenCrystal = 0;
+		yellowCrystal = 0;
+		newNum = 0;
+		scoreNum = 0;
+		$("#results").html(scoreNum);
+		initializeGame();
 	}
-
-	//Resets game if player matches random number and updates wins
-	if (scoreNum === randomNum) {
-		wins+=1;
-		reset();
-	}
+	
 });
